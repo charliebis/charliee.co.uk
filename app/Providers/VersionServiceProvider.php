b2068c51d05e4be8ee537fd1e3d591fef1ca0f05
Charles Edwards <charlie@charliee.co.uk>
2024-01-25 21:25:05 +0000

    Updated gitlab-ci.yml to make .deployment-info file as a JSON encoded array of Gitlab environment variables

--- app/Providers/VersionServiceProvider.php
+++ app/Providers/VersionServiceProvider.php
@@ -18,10 +18,18 @@ class VersionServiceProvider extends ServiceProvider
      * Bootstrap services.
      */
     public function boot(): void {
-        $path = base_path('.version');
+        $path = base_path('.deployment-info');
         if (file_exists($path)) {
-            $version = trim(file_get_contents($path));
-            config(['app.version' => $version]);
+            $deploymentInfo = trim(file_get_contents($path));
+            // Check if the file content is valid JSON
+            json_decode($deploymentInfo);
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $deploymentInfo = json_decode($deploymentInfo, true);
+                // Loop through the resulting array and create Laravel config values
+                foreach($deploymentInfo as $key => $value) {
+                    config(['app.' . strtolower($key) => $value]);
+                }
+            }
         }
         else {
             config(['app.version' => 'Unversioned']);
