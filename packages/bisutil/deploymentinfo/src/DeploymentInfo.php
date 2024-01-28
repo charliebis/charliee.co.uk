1db7a0e8a697c13f70572e6877cc6e43d800a4d1
Charles Edwards <charlie@charliee.co.uk>
2024-01-28 02:04:18 +0000

    Improved DeploymentInfo.php to handle multidimensional arrays in the JSON input file.

--- packages/bisutil/deploymentinfo/src/DeploymentInfo.php
+++ packages/bisutil/deploymentinfo/src/DeploymentInfo.php
@@ -4,6 +4,9 @@ namespace BisUtil\DeploymentInfo;
 
 class DeploymentInfo
 {
+    /**
+     * @return void
+     */
     public function loadDeploymentInfo(): void {
         $path = base_path('.deployment-info.json');
         if (file_exists($path)) {
@@ -12,10 +15,7 @@ class DeploymentInfo
             json_decode($deploymentInfo);
             if (json_last_error() === JSON_ERROR_NONE) {
                 $deploymentInfo = json_decode($deploymentInfo, true);
-                // Loop through the resulting array and create Laravel config values
-                foreach($deploymentInfo as $key => $value) {
-                    config(['app.deploymentinfo.' . strtolower($key) => $value]);
-                }
+                $this->loadConfigsFromJson($deploymentInfo, '');
                 if(!empty($deploymentInfo['CI_COMMIT_SHA'])) {
                     config(['app.deploymentinfo.version' => $deploymentInfo['CI_COMMIT_SHA']]);
                 }
@@ -25,6 +25,27 @@ class DeploymentInfo
         }
         else {
             config(['app.deploymentinfo.version' => 'Unversioned']);
+        }
+    }
+
+
+    /**
+     * Load the configuration values from a JSON array and create Laravel config values.
+     *
+     * @param array  $deploymentInfo   The array containing deployment information.
+     * @param string $base             The base name for the config values.
+     *                                 This will be used as a prefix in the Laravel config keys.
+     *
+     * @return void
+     */
+    protected function loadConfigsFromJson(array $deploymentInfo, string $base): void {
+        // Loop through the resulting array and create Laravel config values
+        foreach($deploymentInfo as $key => $value) {
+            if(is_array($value)) {
+                $this->loadConfigsFromJson($value, strtolower($key) . '.');
+            } else {
+                config(['app.deploymentinfo.' . $base . strtolower($key) => $value]);
+            }
         }
     }
 }
