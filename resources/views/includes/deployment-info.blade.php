ddbc5cedcd5b8ef5730e7b7e1928768c04101c08
Charles Edwards <charlie@charliee.co.uk>
2024-01-30 16:14:01 +0000

    Changed displayed version hash to be the short code

--- resources/views/includes/deployment-info.blade.php
+++ resources/views/includes/deployment-info.blade.php
@@ -1,4 +1,4 @@
 @if(DeploymentInfo::getStatus() === 'success')
-    <p class="mt-6">{{ DeploymentInfo::getDeploymentInfoValueByKey('CI_COMMIT_SHA') }}</p>
+    <p class="mt-6">{{ DeploymentInfo::getDeploymentInfoValueByKey('CI_COMMIT_SHORT_SHA') }}</p>
     {{--<p class="mt-6">{{ print_r(DeploymentInfo::getDeploymentInfo(), true) }}</p>--}}
 @endif
